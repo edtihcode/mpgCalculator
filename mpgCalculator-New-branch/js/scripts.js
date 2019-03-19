bba3cfe9836f6f9d0f69ee2f9784fec2e5e163b4
SkySaber15 <47193581+SkySaber15@users.noreply.github.com>
2019-03-19 12:32:09 -0700

    Add files via upload

--- mpgCalculator-New-branch/js/scripts.js
+++ mpgCalculator-New-branch/js/scripts.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
     var gallonInput = $("#gallon").val();
     var distanceTravelled = $("#miles").val();
     var gasInputValue = $("#gasPrice").val();
-    console.log(gallonInput,distanceTravelled);
-    mpg = calMPG(gallonInput,distanceTravelled);
+    console.log(gallonInput, distanceTravelled);
+    mpg = calMPG(gallonInput, distanceTravelled);
     $("#result1").val(mpg);
-    costPerMile = calCostPerMile(gasInputValue,gallonInput,distanceTravelled);
+    costPerMile = calCostPerMile(gasInputValue, gallonInput, distanceTravelled);
     $("#result2").val(costPerMile);
-    distancePerDollar = calDistancePerDollar(gasInputValue,gallonInput,distanceTravelled);
+    distancePerDollar = calDistancePerDollar(gasInputValue, gallonInput, distanceTravelled);
     $("#result3").val(distancePerDollar);
     //console.log(gallonInput, gasInputValue);
 
-    });
+  });
 
   //   console.log(validateEmail(emailText), passwordText);
   //   $("#error").css("visibility", "visible")
@@ -32,48 +32,51 @@ $(document).ready(function() {
   //   }
   // });
   $("#calculateSavings").click(function() {
-    var priceA= $("#gasPriceA").val();
-    var priceD= $("#gasPriceB").val();
-    var distanceA= $("#aDistance").val();
-    var distanceB= $("#bDistance").val();
+    var priceA = $("#gasPriceA").val();
+    var priceD = $("#gasPriceB").val();
+    var distanceA = $("#aDistance").val();
+    var distanceB = $("#bDistance").val();
     var costA = $("#costToA").val();
     var costB = $("#costToB").val();
     var purchaseG = $("#gallonToPurchase").val();
 
-    mpg = calMPG(gallonInput,distanceTravelled);
+    mpg = calMPG(gallonInput, distanceTravelled);
     $("#result1").val(mpg);
-    costPerMile = calCostPerMile(gasInputValue,gallonInput,distanceTravelled);
+    costPerMile = calCostPerMile(gasInputValue, gallonInput, distanceTravelled);
     $("#result2").val(costPerMile);
-    distancePerDollar = calDistancePerDollar(gasInputValue,gallonInput,distanceTravelled);
+    distancePerDollar = calDistancePerDollar(gasInputValue, gallonInput, distanceTravelled);
     $("#result3").val(distancePerDollar);
     //console.log(gallonInput, gasInputValue);
 
-    });
+  });
 });
-function calMPG(gallonInput,distanceTravelled){
+
+function calMPG(gallonInput, distanceTravelled) {
   mpgvalue = parseInt(distanceTravelled) / parseInt(gallonInput);
   mpg = mpgvalue.toFixed(2) + " mpg";
-    console.log("ran");
+  console.log("ran");
   return mpg;
 
 }
-function calCostPerMile(gasInputValue,gallonInput,distanceTravelled){
-  pricePerMileValue = parseFloat(gasInputValue)/ (parseInt(distanceTravelled) / parseInt(gallonInput));
+
+function calCostPerMile(gasInputValue, gallonInput, distanceTravelled) {
+  pricePerMileValue = parseFloat(gasInputValue) / (parseInt(distanceTravelled) / parseInt(gallonInput));
   costPerMile = "$" + pricePerMileValue.toFixed(2);
-    console.log("ran");
+  console.log("ran");
   return costPerMile;
 }
-function calDistancePerDollar(gasInputValue,gallonInput,distanceTravelled){
-  distancePerDollarValue = (parseInt(distanceTravelled) / parseInt(gallonInput))/parseFloat(gasInputValue);
+
+function calDistancePerDollar(gasInputValue, gallonInput, distanceTravelled) {
+  distancePerDollarValue = (parseInt(distanceTravelled) / parseInt(gallonInput)) / parseFloat(gasInputValue);
   distancePerDollar = distancePerDollarValue.toFixed(2) + " miles";
-    console.log("ran");
+  console.log("ran");
   return distancePerDollar;
 }
 
-function calCostToGetThere(distanceA,costPerMile){
+function calCostToGetThere(distanceA, costPerMile) {
   costValue = parseInt(distanceA) * parseInt(pricePerMileValue);
   cost = "$" + costValue;
-    console.log("ran");
+  console.log("ran");
   return cost;
 
 }
