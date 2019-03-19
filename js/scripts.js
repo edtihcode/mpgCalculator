e6883dd745ffe2d3d9143f89ccc5e3f11c765004
Ed Code <EdCode@Nans-MacBook-Air.local>
2019-03-19 11:46:17 -0700

    working version but rounded

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 console.log("Hello World!");
 $(document).ready(function() {
   var mpg;
-  var cost;
   $("#submit").click(function() {
     var gallonInput = $("#gallon").val();
     var distanceTravelled = $("#miles").val();
     var gasInputValue = $("#gasPrice").val();
-    console.log(gallonInput,distanceTravelled);
+    // console.log(gallonInput,distanceTravelled);
     mpg = calMPG(gallonInput,distanceTravelled);
     $("#result1").val(mpg);
     costPerMile = calCostPerMile(gasInputValue,gallonInput,distanceTravelled);
@@ -33,47 +32,84 @@ $(document).ready(function() {
   // });
   $("#calculateSavings").click(function() {
     var priceA= $("#gasPriceA").val();
-    var priceD= $("#gasPriceB").val();
+    var priceB= $("#gasPriceB").val();
     var distanceA= $("#aDistance").val();
     var distanceB= $("#bDistance").val();
     var costA = $("#costToA").val();
     var costB = $("#costToB").val();
     var purchaseG = $("#gallonToPurchase").val();
 
-    mpg = calMPG(gallonInput,distanceTravelled);
-    $("#result1").val(mpg);
-    costPerMile = calCostPerMile(gasInputValue,gallonInput,distanceTravelled);
-    $("#result2").val(costPerMile);
-    distancePerDollar = calDistancePerDollar(gasInputValue,gallonInput,distanceTravelled);
-    $("#result3").val(distancePerDollar);
-    //console.log(gallonInput, gasInputValue);
+    costToGetToA = calCostToGetThere(distanceA,costPerMile);
+    $("#costToA").val("$" + costToGetToA);
+    purchaseA = purchaseValueG(priceA,purchaseG);
+    totalCostValueA = calTotalCostValue(purchaseValue,costValue);
+    costToGetToB = calCostToGetThere(distanceB,costPerMile);
+    $("#costToB").val("$" + costToGetToB);
+    purchaseB = purchaseValueG(priceB,purchaseG);
+    totalCostValueB = calTotalCostValue(purchaseValue,costValue);
+    totalSavings = compare(totalCostValueA,totalCostValueB);
+    console.log(totalCostValueA+ "for a");
+    console.log(totalCostValueB+ "for b");
+    console.log(totalSavings.value + totalSavings.station);
+    $("#Results").html(`<h1>You will save ${totalSavings.value} by going to station ${totalSavings.station} </h1>`)
 
     });
+
 });
+function calTotalCostValue(purchaseValue,costValue){
+  totalCostValue = purchaseValue + costValue ;
+  return totalCostValue;
+}
+
+function compare(a,b){
+  if (a > b) {
+    savings = a - b;
+    var stationOfChoice = "B";
+    var result = {value: savings, station: stationOfChoice};
+    return result;
+  }else if (b > a) {
+    savings = b - a;
+    var stationOfChoice = "A";
+    var result = {value: savings, station: stationOfChoice};
+    return result;
+  }else {
+    var stationOfChoice = "No difference/cannot be computed";
+    var result = {value: "", station: stationOfChoice};
+    return result;
+  }
+
+}
+
+function purchaseValueG(price,purchaseG){
+  purchaseValue = parseInt(price) * parseInt(purchaseG);
+  return purchaseValue;
+}
+
 function calMPG(gallonInput,distanceTravelled){
   mpgvalue = parseInt(distanceTravelled) / parseInt(gallonInput);
   mpg = mpgvalue.toFixed(2) + " mpg";
-    console.log("ran");
+    // console.log("ran");
   return mpg;
 
 }
-function calCostPerMile(gasInputValue,gallonInput,distanceTravelled){
-  pricePerMileValue = parseFloat(gasInputValue)/ (parseInt(distanceTravelled) / parseInt(gallonInput));
-  costPerMile = "$" + pricePerMileValue.toFixed(2);
-    console.log("ran");
-  return costPerMile;
-}
+
 function calDistancePerDollar(gasInputValue,gallonInput,distanceTravelled){
   distancePerDollarValue = (parseInt(distanceTravelled) / parseInt(gallonInput))/parseFloat(gasInputValue);
   distancePerDollar = distancePerDollarValue.toFixed(2) + " miles";
-    console.log("ran");
+    // console.log("ran");
   return distancePerDollar;
 }
 
 function calCostToGetThere(distanceA,costPerMile){
   costValue = parseInt(distanceA) * parseInt(pricePerMileValue);
-  cost = "$" + costValue;
-    console.log("ran");
+  cost =  costValue;
+    //console.log(cost);
   return cost;
+}
 
+function calCostPerMile(gasInputValue,gallonInput,distanceTravelled){
+  pricePerMileValue = parseFloat(gasInputValue)/ (parseInt(distanceTravelled) / parseInt(gallonInput));
+  costPerMile = "$" + pricePerMileValue.toFixed(2);
+    console.log("ran");
+  return costPerMile;
 }
